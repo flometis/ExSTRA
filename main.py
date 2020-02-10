1618b4ef7e4e1742ece4d4a9d678df90dcf1d3a5
Floriana Sciumbata <46404315+flometis@users.noreply.github.com>
2020-02-10 01:25:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import subprocess
+
+#Gli argomenti forniti allo script sono contenuti nella lista sys.argv, e il primo elemento della lista è sempre il percorso completo dell'eseguibile. Quindi per scoprire la cartella in cui ci troviamo usiamo la funzione os.path.dirname (che ti fornisce il nome della cartella del file che le dai in pasto). Aggiungiamo os.path.abspath per essere sicuri che il risultato sia un percorso valido.
+import os.path
+import sys
+import time
+import os
+import platform
+
+#VARIABILI GLOBALI
+
+platform.system
+if  "Windows":
+    eseguibile = os.path.abspath(os.path.dirname(sys.argv[0])) + "/bin-Win64/udpipe"  
+elif "Linux":
+    eseguibile = os.path.abspath(os.path.dirname(sys.argv[0])) + "/bin-linux64/udpipe"
+elif "Mac":
+    eseguibile = os.path.abspath(os.path.dirname(sys.argv[0])) + "/bin-osx/udpipe"
+	#È chiaro che su Windows dovresti modificare il nome della cartella in bin-win64
+#TODO: si può usare la funzione platform.system per riconoscere il sistema operativo (https://stackoverflow.com/a/1857). Si possono usare le condizioni if per creare il giusto percorso dell'eseguibile su ogni sistema.
+modello = os.path.abspath(os.path.dirname(sys.argv[0])) + "/modelli/italian-isdt-ud-2.4-190531.udpipe"
+
+dct = {"sindex" : 0, "tkn" : 1, "lemma" : 2, "POS" : 3, "RPOS" : 4, "morph" : 5 , "depA": 6, "depB": 7}
+
+def patternfinder(corpus, patternlist):
+    global eseguibile
+    global modello
+    global dct
+    #Udpipe andrebbe lanciato con un comando del tipo ./bin-linux64/udpipe --tokenize --tag --parse ./modelli/italian-isdt-ud-2.4-190531.udpipe
+    #Per poterlo avviare da Python con la libreria Subprocess dobbiamo dividere i vari argomenti in elementi di una lista, così non ci sono spazi
+    process = subprocess.Popen([eseguibile, "--tokenize", "--tag", "--parse", modello], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #Per lavorare sullo standard input bisogna usare una sequenza di byte invece di una comune stringa di testo, perché siamo a basso livello. Una stringa può essere codificata come byte usando la sua funzione encode
+    testobyte = corpus.encode(encoding='utf-8')
+    #Una volta si usava questo metodo per scrivere su stdin:
+    #process.stdin.write(testo)
+    #ma ci sono dei problemi con i testi lunghi. Adesso si usa direttamente la funzione communicate, che allo stesso tempo fornisce anche la risposta elaborata dall'eseguibile
+    outputbyte = process.communicate(testobyte)[0]
+    #È una buona idea chiudere il flusso dello stdin quando hai finito di scrivere
+    process.stdin.close()
+    #Chiaramente anche l'output è una sequenza di byte, basta decodificarli in una stringa con la stessa codifica utf-8 usata per l'input
+    stroutput = outputbyte.decode(encoding='utf-8')
+    #print(stroutput)
+    
+    listarisultati = []
+    mytable = stroutput.split("\n")
+    for row in range(len(mytable)):
+        mytable[row] = mytable[row].split("\t") #creare lista di liste (tabella, riga, colonna)
+        if len(mytable[row]) <8:
+            continue
+        for pattern in patternlist: 
+            if pattern in mytable[row][dct["lemma"]]:
+                listarisultati.append(mytable[row][dct["lemma"]])
+        
+    return listarisultati
+
+corpus = sys.argv[1]
+patternlist = sys.argv[2].split(",")
+print(patternlist)
+risultato = patternfinder(corpus, patternlist)
+print(risultato)
