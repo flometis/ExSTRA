35f19aed58d90646049f41e9b529e40988a27514
Flo <flo.metis@gmail.com>
2021-11-29 23:10:09 +0100

    Pulizia automatica testi

---
+++ pulisci.py
@@ -0,0 +1,43 @@
+import re
+import os
+import os.path
+import sys
+import platform
+
+path = os.path.abspath(os.path.dirname(sys.argv[0])) + "/Eltec100/Eltec100"
+
+so = platform.system()
+if so == "Windows":
+    path = path.replace("/", "\\")
+
+filenames = [os.path.abspath(path + "/" + filename) for filename in os.listdir(path)]
+
+toDo = [
+    {"find": "[\r\n]+", "rep": "\n", "flag": "", "tag": ""}, 
+    {"find": "([^\.\!\?]) *\n", "rep": "\g<1>", "flag": "", "tag": ""},
+    ]
+
+for filename in filenames:
+    so = platform.system()
+    if so == "Windows":
+        filename = filename.replace("/", "\\")
+    text_file = open(filename, "r", encoding='utf-8')
+    fulltext = text_file.read()
+    text_file.close()
+    print("Sto lavorando su: " + filename)
+    
+    i = 0
+    for operazione in toDo:
+        i = i + 1
+        print(str((i/len(toDo))*100) + "%")
+        fulltext = re.sub(operazione["find"], operazione["rep"],  fulltext)
+        
+    
+    output = os.path.abspath(os.path.dirname(sys.argv[0])) + "/Eltec100/puliti/" + os.path.basename(filename)
+    so = platform.system()
+    if so == "Windows":
+        output = output.replace("/", "\\")
+    text_file = open(output, "w", encoding='utf-8')
+    text_file.write(fulltext)
+    text_file.close()
+    
