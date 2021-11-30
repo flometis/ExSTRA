4e1773af1bc18f1e0768b4c7e613b2d4a5cd8427
Flo <flo.metis@gmail.com>
2021-12-01 00:01:53 +0100

    Aggiunta e correzione regex pulisci.py

--- pulisci.py
+++ pulisci.py
@@ -15,27 +15,33 @@ filenames = [os.path.abspath(path + "/" + filename) for filename in os.listdir(p
 toDo = [
     {"find": "\r", "rep": "\n", "flag": 0, "tag": "carriageReturn"},
     {"find": "\n[\n]+", "rep": "\n", "flag": 0, "tag": "inviiMultipli"},
-    {"find": "^\s*", "rep": "", "flag": re.MULTILINE, "tag": "spazioInizioRiga"},
+    {"find": "^\s+", "rep": "", "flag": re.MULTILINE, "tag": "spazioInizioRiga"},
+    {"find": "[Dd]igitized\s*\n+\s*by\s*\n*\s*[Gg]oogle", "rep": "", "flag": 0, "tag": "digitizedByGoogle"},
+    {"find": "http(s)*:\/\/[^\s]", "rep": "", "flag": 0, "tag": "url"},
+    {"find": "^[^A-Za-z0-9èéàìùíúáóòÁÀÈÉÍÌÓÒÚÙ]+$", "rep": "", "flag": re.MULTILINE, "tag": "righeSenzaTxt"},
     {"find": "([^\s])\-\s*\n", "rep": "\g<1>", "flag": 0, "tag": "paroleSpezzate"},
-    {"find": "\s*\#+.*\#+\s*", "rep": "", "flag": re.MULTILINE, "tag": "numeroPaginaCancelletto"},
-    {"find": "([^\.\!\?\n\s])\s*\n", "rep": "\g<1>", "flag": 0, "tag": "invioAMetaFrase"},
+    {"find": "#+[^\n]+#+", "rep": "", "flag": 0, "tag": "numeroPaginaCancelletto"},
+    {"find": "^\**(-|—|_|~)+ *[0-9]+ *(-|—|_|~)* *;*:*", "rep": "", "flag": re.MULTILINE, "tag": "numeroPaginatrattino"},
+    {"find": "([^\.\!\?\n\s])\s*\n", "rep": "\g<1> ", "flag": 0, "tag": "invioAMetaFrase"},
+    {"find": "\s\s+", "rep": " ", "flag": 0, "tag": "spaziMultipli"},
+    {"find": "_", "rep": "", "flag": 0, "tag": "underscore"},
     ]
 
 for filename in filenames:
     so = platform.system()
     if so == "Windows":
         filename = filename.replace("/", "\\")
+    print("Sto lavorando su: " + filename)
     text_file = open(filename, "r", encoding='utf-8')
     fulltext = text_file.read()
     text_file.close()
-    print("Sto lavorando su: " + filename)
     
     i = 0
     print("0%", end='')
     for operazione in toDo:
         i = i + 1
         print('\r'+str(int((i/len(toDo))*100)) + "%", end='', flush=True)
-        if "numeroPaginaCancelletto" in operazione["tag"] and not "La lega lombarda" in filename:
+        if "numeroPaginaCancelletto" in operazione["tag"] and not bool("lega-lombarda" in filename or "Torricelli" in filename or "Silvola" in filename or "assedio-di-Firenze" in filename):
             #Per ora eseguiamo questa operazione solo su questo file, vedremo se fa danni su altri file
             continue
         fulltext = re.sub(operazione["find"], operazione["rep"],  fulltext, flags=operazione["flag"])
