bc35d949296d79a08f891b0583d87e9e94ea3e6c
Flo <flo.metis@gmail.com>
2022-03-06 22:47:36 +0100

    Fix valore None in dividiRisultati.py

--- dividiRisultati.py
+++ dividiRisultati.py
@@ -3,7 +3,7 @@ import os.path
 import sys
 import platform
 
-tagDct = {"profession" : "professions.tsv", "nobiliary particle": None, "noble rank": "nobles.tsv", "honorary title": "Q3320743", "ecclesiastical occupation": "professions.tsv", "historical profession": "Q16335296", "military rank": "Q56019", "human": "Q5", "factory (place)": "Q83405", "facility (place)": "Q13226383", "workplace (place)": "Q628858", "shop (place)": "Q213441", "commercial building (place)": "Q655686", "religious building (place)": "Q24398318", "house (place)": "Q3947", "rural building (place)": "Q131596", "fortification (place)": "Q57821", "venue (place)": "Q17350442"}
+tagDct = {"profession" : "professions.tsv", "nobiliary particle": None, "noble rank": "nobles.tsv", "honorary title": None, "ecclesiastical occupation": "professions.tsv", "historical profession": "professions.tsv", "military rank": "professions.tsv", "human": None, "factory (place)": "places-industrial.tsv", "facility (place)": "Q13226383", "workplace (place)": "Q628858", "shop (place)": "places-commercial.tsv", "commercial building (place)": "places-commercial.tsv", "house (place)": "places-civil.tsv", "rural building (place)": "places-rural.tsv", "fortification (place)": "places-military.tsv", "venue (place)": "Q17350442", "religious building (place)": "places-religious.tsv", "industrial (place)": "places-industrial.tsv", "commercial building (place)": "places_commercial.tsv", "civil building (place)" : "places-civil.tsv", "public (place)": "places-public.tsv"}
 
 findingsfile = os.path.abspath(os.path.dirname(sys.argv[0]))+"/Findings/exstra_dictionary_COMPLETE.tsv"
 text_file = open(findingsfile, "r", encoding='utf-8')
@@ -11,22 +11,33 @@ findingsstr = text_file.read()
 text_file.close()
 
 for k in tagDct:
-    if k == None:
+    if tagDct[k] == None:
         continue
     text_file = open(tagDct[k], "w", encoding='utf-8')
     text_file.write("")
     text_file.close()
-    
+text_file = open("places.tsv", "w", encoding='utf-8')
+text_file.write("")
+text_file.close()
 
 for line in findingsstr.split("\n"):
-    tag = line.split("\t")[6]
+    try:
+        tag = line.split("\t")[6]
+    except:
+        continue
+    filename = None
     for k in tagDct:
-        if k == None:
+        if tagDct[k] == None:
             continue
-        if k in tag:
+        if k == tag:
             filename = tagDct[k]
             break
-    with open(filename, "a") as file_object:
-        file_object.write(line + "\n")
-            
-      
+    try:
+        with open(filename, "a") as file_object:
+            file_object.write(line + "\n")
+        if filename != "professions.tsv":
+            with open("places.tsv", "a") as file_object:
+                file_object.write(line + "\n") 
+    except:
+        pass
+             
